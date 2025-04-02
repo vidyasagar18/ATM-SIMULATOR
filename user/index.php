7316367b4429247eb33844f8a30fba4d40cffebd
vidyasagar18 <163834738+vidyasagar18@users.noreply.github.com>
2025-04-02 22:59:35 +0530

    Create index.php

---
+++ user/index.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>ATM Simulator</title></head>
+<body>
+    <h2>Welcome to the ATM</h2>
+    <a href="deposit.php">Deposit</a>
+    <a href="withdraw.php">Withdraw</a>
+    <a href="transfer.php">Transfer</a>
+    <a href="history.php">Transaction History</a>
+</body>
+</html>
